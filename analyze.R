0b9c3c60b57eb21fe498bbc90b2103f84593057d
Daniel Janus <dj@danieljanus.pl>
2016-08-03 23:16:36 +0200

    analyze: detect inactivity periods.

--- analyze.R
+++ analyze.R
@@ -1,6 +1,25 @@
 library(ggplot2)
 library(scales)
 
+find.inactivity <- function(df, threshold = 5 * 60) {
+    r <- rle(df$activity)
+    pos <- which(r$lengths >= threshold & !r$values)
+    ofs <- c(0, cumsum(r$lengths))
+    res <- data.frame(start = ofs[pos] + 1,
+                      count = r$lengths[pos])
+    res$starttime <- df$timestamp[res$start]
+    res$length <- difftime(df$timestamp[res$start + res$count - 1], df$timestamp[res$start])
+    res
+}
+
+remove.inactivity <- function(df, inactive = find.inactivity(df)) {
+    rows.to.remove <- integer()
+    for (i in 1:nrow(inactive)) {
+        rows.to.remove <- c(rows.to.remove, inactive$start[i]:(inactive$start[i] + inactive$count[i] - 1))
+    }
+    df[-rows.to.remove,]
+}
+
 read.snafu <- function(windows.file = "windows.dat", strings.file = "strings.dat") {
     f <- file(windows.file, "rb")
     f2 <- file(strings.file, "rb")
@@ -19,11 +38,22 @@ read.snafu <- function(windows.file = "windows.dat", strings.file = "strings.dat
 categorize <- function(df) {
     df$category = "Unknown"
     df$category[df$window == "Terminal"] <- "Terminal"
+    df$category[grepl("Facebook", df$window)] <- "Facebook"
     df$category[grepl("Chromium$", df$window)] <- "Other browser"
+    df$category[grepl("Firefox$", df$window)] <- "Other browser"
+    df$category[grepl("^emacs", df$window)] <- "Emacs"
     df
 }
 
 df <- categorize(read.snafu())
 
-ggplot(df, aes(x = category, fill = category)) + geom_bar(aes(y = ..count.. / sum(..count..))) + theme_bw() + scale_y_continuous(labels = percent) + xlab("Category") + ylab("Window")
+plot.activity <- function(df) {
+    ggplot(df, aes(x = category, fill = category)) +
+        geom_bar(aes(y = ..count.. / sum(..count..))) +
+        theme_bw() +
+        scale_y_continuous(labels = percent) +
+        xlab("Category") +
+        ylab("Window")
+}
 
+plot.activity(remove.inactivity(df))
